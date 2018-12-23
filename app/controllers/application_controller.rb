a3b08e04d16ec44eb4554f49fdece8f995c8ef30
Rachel Wyatt <rw126@evansville.edu>
2018-12-23 20:08:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class App < Sinatra::Base
 
     post '/teams' do
       @team=Team.new(params["team"]["name"], params["team"]["motto"])
+
+      params["team"]["hero"].each do |data|
+        Hero.new(data["name"], data["power"], data["bio"])
+      end
       erb :team
     end
 

95c0aec9baa7a86bd69b3ecbbfbfc6cde7f6a959
Rachel Wyatt <rw126@evansville.edu>
2018-12-23 20:16:13 +0000

    Automatically backed up by Learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,16 +1,16 @@
-class Team 
-  attr_accessor :name, :motto 
+class Team
+  attr_accessor :name, :motto
 
   @@all = []
 
   def initialize(name, motto)
-    @name = name 
+    @name = name
     @motto = motto
     @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 
 

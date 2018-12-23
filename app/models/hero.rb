9145f5df84e43a03dbe9e6fa0ba8db0ed864ff38
Rachel Wyatt <rw126@evansville.edu>
2018-12-23 20:05:26 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,8 +2,8 @@ class Hero
   attr_accessor :name, :power, :bio
 
   def initialize(name, power, bio)
-    @name = name 
-    @power = power 
+    @name = name
+    @power = power
     @bio = bio
 
   end
